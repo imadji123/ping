37c882f310915a887aebcbe0eed9e54f4ddd4e23
Adji Zulfiqar Fauzi <imadjistudio@gmail.com>
2018-04-19 15:27:10 +0700

    Setup firebase messaging service

---
+++ app/src/main/java/com/imadji/ping/MyFirebaseInstanceIdService.java
@@ -0,0 +1,17 @@
+package com.imadji.ping;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String TAG = MyFirebaseInstanceIdService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "TOKEN: " + refreshedToken);
+    }
+
+}
---
+++ app/src/main/java/com/imadji/ping/MyFirebaseMessagingService.java
@@ -0,0 +1,16 @@
+package com.imadji.ping;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        String message = remoteMessage.getNotification().getBody();
+        Log.d(TAG, "MESSAGE: " + message);
+    }
+}
